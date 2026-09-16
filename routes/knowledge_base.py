6f346f6ad3d343f0242840348908cb76ae206a96
wangys38-cyber <wangys38-cyber@users.noreply.github.com>
2026-09-16 14:05:26 +0800

    fix: 修复缩进语法错误导致服务启动失败

--- routes/knowledge_base.py
+++ routes/knowledge_base.py
@@ -726,7 +726,7 @@ def mic_stop():
         
         # whisper识别
         os.environ['HF_ENDPOINT'] = 'https://hf-mirror.com'
-os.environ['HF_HUB_DISABLE_XET'] = '1'
+        os.environ['HF_HUB_DISABLE_XET'] = '1'
         from faster_whisper import WhisperModel
         if not hasattr(mic_stop, '_model'):
             mic_stop._model = WhisperModel('tiny', device='cpu', compute_type='int8')
